679ea9514af37f4f5c9ddd24178a774df11e7091
Filip <el.filberto@gmail.com>
2022-11-15 15:30:33 +0100

    First and final init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Welcome to the most common made game ever!");
+    println!("The Guess my number Game!");
+    println!("Let's generate a number to guess.. READY!");
+
+    let number_to_guess = rand::thread_rng().gen_range(1..=100);
+    let mut counter = 0;
+
+    loop {
+        println!("Please type in your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("That is not a number, please try again.");
+                continue;
+            }
+        };
+    
+        println!("You guessed: {guess}");
+        counter += 1;
+
+        match guess.cmp(&number_to_guess) {
+            Ordering::Less => println!("Higher!"),
+            Ordering::Greater => println!("Lower!"),
+            Ordering::Equal => {
+                println!("Bingo! ({counter} atempts)");
+                break;
+            }
+        }
+    }
+}
